56c4593d91ba61b128541e005ccb0093bf7e8721
turnerth <turnerth@ge.com>
2017-05-22 13:02:23 -0700

    Update quickstart-front-end-template.sh

--- scripts/quickstart-front-end-template.sh
+++ scripts/quickstart-front-end-template.sh
@@ -86,8 +86,11 @@ function init() {
   #get the script that reads version.json
   eval "$(curl -s -L $IZON_SH)"
 
+  echo "getVersionFile"
   getVersionFile
+  echo "gotVersionFile"
   getLocalSetupFuncs
+  echo "gotLocalSetupFuncs"
 }
 
 if [[ $PRINT_USAGE == 1 ]]; then
